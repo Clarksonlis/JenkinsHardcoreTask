5d739e8ef0be48da3bbaf95cac2886e14ba37e0a
daryakaptsiuh <clarksonlis1999@gmail.com>
2023-07-03 21:19:08 +0400

    Add Project

---
+++ HardcoreTask/Hardcore/Tests/BaseTestClass.cs
@@ -0,0 +1,50 @@
+﻿using Hardcore.Tests.Bots;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using Hardcore.Tests.Configuration;
+
+namespace Hardcore.Tests.Tests
+{
+    [TestFixture]
+    public class BaseTestClass
+    {
+        protected IWebDriver _driver;
+        protected ActionBot _actionBot;
+        protected DefaultWait<IWebDriver> _wait;
+        protected ITakesScreenshot takesScreenshot;
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            this._driver = GetDriver();
+            this._wait = this.GetWaiter();
+            this._actionBot = this.GetActionBot();
+            this._driver.Manage().Window.Maximize();
+            takesScreenshot = (ITakesScreenshot)this._driver;
+        }
+
+        protected IWebDriver GetDriver()
+        {
+            BrowserType browserType = Configuration.Configuration.ReadBrowserTypeFromConfig();
+            return Managers.WebDriverManager.GetWebDriver(browserType);
+        }
+
+        protected DefaultWait<IWebDriver> GetWaiter()
+        {
+            return new WebDriverWait(_driver, TimeSpan.FromSeconds(50));
+        }
+
+        protected ActionBot GetActionBot()
+        {
+            return new ActionBot(this._driver, this._wait);
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            // Завершение работы WebDriver
+            _driver.Quit();
+        }
+    }
+}
